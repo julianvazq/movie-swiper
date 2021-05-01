c308186a04af296a1f39167098cbe3406f78949c
Julian Vazquez <julianvazquez101@gmail.com>
2021-05-01 18:44:01 -0400

    Add controllers folder

---
+++ server/src/controllers/movies.ts
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch';
+
+const getMovieById = async (req, res) => {
+    try {
+        // Destructure data from client
+        const { id } = req.params;
+
+        const data = await fetch(
+            `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&append_to_response=videos,images`
+        );
+        const movie = await data.json();
+        res.json(movie);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Request failed!' });
+    }
+};
+
+const getMovieQuery = async (req, res) => {
+    try {
+        const { query } = req.params;
+
+        const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.API_KEY}&language=en-US&query=${query}&append_to_response=images`;
+        const data = await fetch(url);
+        const movie = await data.json();
+        res.json(movie);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Request failed!' });
+    }
+};
+
+const getGenres = async (req, res) => {
+    try {
+        const { genres = [], page = '1' } = req.body;
+        const genreIds = genres.join(',');
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreIds}`;
+        const data = await fetch(url);
+        const movies = await data.json();
+        res.json(movies);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Request failed!' });
+    }
+};
+
+const getRandomMovie = async (req, res) => {
+    try {
+        const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`;
+        const response = await fetch(url);
+        console.log(response);
+        const data = await response.json();
+        const numResults = data.results.length;
+        if (!numResults) {
+            return res.json(null);
+        }
+        const movie = data.results[Math.floor(Math.random() * numResults) + 1];
+        res.json(movie);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Request failed!' });
+    }
+};
+
+module.exports = { getGenres, getMovieQuery, getRandomMovie, getMovieById };
--- server/src/routes/movies.ts
+++ server/src/routes/movies.ts
@@ -1,85 +1,34 @@
 import express from 'express';
-import fetch from 'node-fetch';
 const router = express.Router();
+const {
+    getMovieById,
+    getMovieQuery,
+    getGenres,
+    getRandomMovie,
+} = require('../controllers/movies');
 
 /**
  * @route POST /movies/genre
  * @desc Get individual movie details
  */
-router.post('/genres', async (req, res) => {
-    try {
-        const { genres = [], page = '1' } = req.body;
-        const genreIds = genres.join(',');
-
-        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreIds}`;
-        const data = await fetch(url);
-        const movies = await data.json();
-        res.send(movies);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ error: 'Request failed!' });
-    }
-});
+router.post('/genres', getGenres);
 
 /**
- * @route POST /movies/random
+ * @route GET /movies/random
  * @desc Get individual random movie details
  */
-router.get('/random', async (req, res) => {
-    try {
-        const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`;
-        const response = await fetch(url);
-        console.log(response);
-        const data = await response.json();
-        const numResults = data.results.length;
-        if (!numResults) {
-            res.send(null);
-            return;
-        }
-        const movie = data.results[Math.floor(Math.random() * numResults) + 1];
-        res.send(movie);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ error: 'Request failed!' });
-    }
-});
+router.get('/random', getRandomMovie);
 
 /**
  * @route GET /movies/:id
  * @desc Get movie details
  */
-router.get('/:id', async (req, res) => {
-    try {
-        // Destructure data from client
-        const { id } = req.params;
-
-        const data = await fetch(
-            `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&append_to_response=videos,images`
-        );
-        const movie = await data.json();
-        res.send(movie);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ error: 'Request failed!' });
-    }
-});
+router.get('/:id', getMovieById);
 
 /**
  * @route GET /movies/query/:query
  * @desc Get individual movie details
  */
-router.get('/query/:query', async (req, res) => {
-    try {
-        const { query } = req.params;
-
-        const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.API_KEY}&language=en-US&query=${query}&append_to_response=images`;
-        const data = await fetch(url);
-        const movie = await data.json();
-        res.send(movie);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ error: 'Request failed!' });
-    }
-});
+router.get('/query/:query', getMovieQuery);
 
 export default router;
